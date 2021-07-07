1d60fb82b016978384f480e8566eaeb13e3da4b8
mazqt <jesperhipersson@gmail.com>
2021-07-08 00:00:20 +0200

    Forgot to change text

--- src/components/Score.js
+++ src/components/Score.js
@@ -1,9 +1,9 @@
 function Score(props) {
   return (
     <div>
-      <h1>I'm the score</h1>
+      <h1>Score: </h1>
       <h1>{props.score}</h1>
-      <h1>I'm the highscore</h1>
+      <h1>Highscore: </h1>
       <h1>{props.highscore}</h1>
     </div>
   );
